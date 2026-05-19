0a0457898fd342b602a6b71be775fe11ed2263b0
TempusAozora <aozoratenpusu@gmail.com>
2026-05-19 10:56:16 +0800

    Added modify command

--- ProcessCommands.java
+++ ProcessCommands.java
@@ -1,11 +1,15 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.rmi.server.ObjID;
 import java.util.*;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.FileReader;
+import java.io.FileWriter;
 
+// Entry point of command processing
 public class ProcessCommands {
     public String process(String str) {
         Token[] tokens;
@@ -18,8 +22,9 @@ public class ProcessCommands {
         String cmd = tokens[0].value.toUpperCase();
 
         try { // run the method from input command.
-            Method method = Command.class.getDeclaredMethod(cmd, String.class, Token[].class);
-            Object res = method.invoke(null, cmd, tokens);
+            Method method = Command.class.getDeclaredMethod(cmd,Token[].class, TSV.class);
+            TSV tsvData = new TSV("./saved/TableDataSample.tsv");
+            Object res = method.invoke(null, tokens, tsvData);
             return (res instanceof String) ? (String) res : cmd + " Success";
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace(); 
@@ -28,7 +33,7 @@ public class ProcessCommands {
     }
 }
 
-// getColumnLengths(): For display command. From array of indices, get the length of each column for display.
+// Exclusively for display command.
 class Formatter {
     static String createRowLine(String str1, String str2, int[] lengths) { // for formatting row line
         int excess = 2;
@@ -58,11 +63,10 @@ class Formatter {
     }
 }
 
-// Update: class Parser -> class Token
-// Break command into chunks or tokens to parse like a language
-// Also detects syntax errors in user input
+// Breaks command into chunks or tokens to parse like a language
+// Array of tokens are used to properly detect syntax errors and be more flexible.
 // Example input: DISPLAY student_id, full_name WHERE full_name=foo"
-// Output: [
+// Output from Token.tokenize(input): [
 //     Token("COMMAND", "DISPLAY"),
 //     Token("IDENTIFIER", "student_id"),
 //     Token("COMMA", ","),
@@ -76,6 +80,7 @@ class Formatter {
 // COMMAND is the first word in the input.
 // IDENTIFIER is a keyword that represents the column name.
 // OPERATOR are operators such as the equal sign
+// STRINGS are enclosed by quotation marks "" or ''
 class Token {
     public String type;
     public String value;
@@ -173,13 +178,15 @@ class Token {
 class TSV {
     public List<String[]> data;
     public String[] header;
+    public String filePath;
 
     TSV(String filePath) { // initialize TSV
         this.data = new ArrayList<>();
+        this.filePath = filePath;
         BufferedReader br;
 
         try {
-            br = new BufferedReader(new FileReader(filePath));
+            br = new BufferedReader(new FileReader(this.filePath));
             this.header = br.readLine().split("\t", -1);
 
             String line;
@@ -192,36 +199,77 @@ class TSV {
         }
     }
 
-    public int getHeaderIdx(String headerName) {
+    public int getHeaderIdx(String headerName) { // helper function to convert string header to index. If not found return -1.
         for (int i = 0; i < this.header.length; i++) {
             if (this.header[i].equals(headerName)) return i; 
         }
         return -1;
     }
+
+    public void modify(Object[][] modifiedRows) { // format is [[rowIdx, columnIdx[], values[]]
+        BufferedWriter bw;
+        String headers = String.join("\t", this.header); // create one string with headers separated by tabs
+        int i_modified = 0; // additional pointer to track current progress of modifiedRows
+
+        try {
+            bw = new BufferedWriter(new FileWriter(this.filePath));
+            bw.write(headers); // write headers
+
+            for (int i = 0; i < data.size(); i++) {
+                bw.newLine(); // new line every start
+
+                String[] rowData = this.data.get(i);
+                
+                // modify row data
+                int mRowIdx = -1;
+                if (i_modified < modifiedRows.length) mRowIdx = (int) modifiedRows[i_modified][0];
+                if (i == mRowIdx) { // if index matches, modify the rowData
+                    int[] mColumnIdx = (int[]) modifiedRows[i_modified][1]; // get column idx to be modified
+                    String[] mValues = (String[]) modifiedRows[i_modified][2]; // get values
+
+                    for (int j = 0; j < mColumnIdx.length; j++) {
+                        int mIdx = mColumnIdx[j];
+                        String mValue = mValues[j];
+
+                        rowData[mIdx] = mValue; // Modify
+                    }
+                    i_modified++;
+                }
+
+                // write row
+                String row = String.join("\t", rowData);
+                bw.write(row);
+            }
+
+            bw.flush();
+            bw.close();
+        } catch(IOException e) {
+            System.out.print(e);
+        }
+        
+    }
 }
 
 
 class Command {
-    static void ENROLL(String cmd, Token[] tokens) {
+    static void ENROLL(Token[] tokens, TSV tsvData) {
         // Add logic here
     }
-    static void UNENROLL(String cmd, Token[] tokens) {
+    static void UNENROLL(Token[] tokens, TSV tsvData) {
         // Remove logic here
     }
 
-    static String HELP(String cmd, Token[] tokens) {
+    static String HELP(Token[] tokens, TSV tsvData) {
         return "\n" +
             "DISPLAY [header_name, ... || *] WHERE [header_name = \"value\", ...] - displays the table with given conditions. To show the full table, type 'DISPLAY *'" + "\n" +
             "ENROLL (work in progress)" + "\n" +
             "UNENROLL (work in progress)" + "\n";
     }
 
-    static String DISPLAY(String cmd, Token[] tokens) {
-        if (tokens.length == 0) return "Argument/s needed for DISPLAY command";
+    static String DISPLAY(Token[] tokens, TSV tsvData) {
+        if (tokens.length == 1) return "Argument/s needed for DISPLAY command";
 
         // Parse token and check for syntax errors.
-        TSV tsvData = new TSV("./saved/TableDataSample.tsv");
-        
         List<Integer> headerList = new ArrayList<Integer>();
         List<Object[]> filterRow = new ArrayList<Object[]>();
 
@@ -242,7 +290,7 @@ class Command {
                 if ( // detect invalid syntax after comma
                     i_parser < tokens.length &&
                     !(tokens[i_parser].type == "IDENTIFIER" || tokens[i_parser].type == "STAR")
-                ) return "Invalid syntax near DISPLAY. Invalid input after a comma.";
+                ) return "Invalid syntax near DISPLAY. Trailing comma.";
             } else break;
         }
 
@@ -284,7 +332,7 @@ class Command {
                     if ( // detect invalid syntax after comma
                         i_parser < tokens.length &&
                         !(tokens[i_parser].type == "IDENTIFIER")
-                    ) return "Invalid syntax near WHERE. Invalid input after a comma.";
+                    ) return "Invalid syntax near WHERE. Trailing comma.";
                 } else break;
             }
         }
@@ -340,7 +388,128 @@ class Command {
         return "";
     }
 
-    static String MODIFY(String cmd, Token[] tokens) {
-        return "(Work in progress)";
+    static String MODIFY(Token[] tokens, TSV tsvData) {
+        if (tokens.length == 1) return "Argument/s needed for MODIFY command";
+        // Parse token and check for syntax errors.
+        int i_parser = 1; // start at 1
+
+        List<Object[]> Values = new ArrayList<Object[]>(); // [[columnIdx, value]] for setting values
+        List<Object[]> filterRow = new ArrayList<Object[]>(); // [[columnIdx, value]] for filtering rows
+
+        // MODIFY command uses while loop just like WHERE, but slightly modified
+        while (i_parser < tokens.length && tokens[i_parser].type == "IDENTIFIER") { // Parse identifiers and commas
+            if (i_parser+2 >= tokens.length) return "Invalid syntax near MODIFY. Incomplete input";
+
+            // example: student_id = 1
+            String identifier = tokens[i_parser].value; 
+            String equalSign = tokens[i_parser+1].value;
+            String value = tokens[i_parser+2].value;
+
+            if (!equalSign.equals("=")) return "Invalid syntax near MODIFY. Use equal sign.";
+            int idx = tsvData.getHeaderIdx(identifier);
+
+            if (!( // catch invalid type
+                tokens[i_parser+2].type.equals("STRING") || 
+                tokens[i_parser+2].type.equals("NUMBER")
+            )) return "Invalid syntax near MODIFY. Invalid input. Make sure to use numbers or enclose characters in quotation marks.";
+            
+            if (idx == -1) System.out.println("header \"" + identifier + "\" not found. Skipping.");
+            else { // check first if value is a string or number. Then add.
+                Object[] setValues = new Object[2];
+                setValues[0] = idx;
+                setValues[1] = value;
+
+                Values.add(setValues);
+            }
+
+            i_parser += 3;
+            if (i_parser < tokens.length && tokens[i_parser].type == "COMMA") {
+                i_parser++;
+                if ( // detect invalid syntax after comma
+                    i_parser < tokens.length &&
+                    !(tokens[i_parser].type == "IDENTIFIER")
+                ) return "Invalid syntax near WHERE. Trailing comma.";
+            } else break;
+        }
+
+        if (Values.size() == 0) return "No valid header data";
+        if (i_parser < tokens.length) { 
+            if (!tokens[i_parser].type.equals("WHERE")) 
+                return "Invalid syntax after DISPLAY. Use WHERE to filter rows.";
+            i_parser++;
+            if (i_parser >= tokens.length) return "Invalid syntax after WHERE. Empty field.";
+
+            while (i_parser < tokens.length && tokens[i_parser].type == "IDENTIFIER") { // Parse WHERE clause
+                if (i_parser+2 >= tokens.length) return "Invalid syntax near WHERE. Incomplete input";
+
+                // example: student_id = 1
+                String identifier = tokens[i_parser].value; 
+                String equalSign = tokens[i_parser+1].value;
+                String value = tokens[i_parser+2].value;
+
+                if (!equalSign.equals("=")) return "Invalid syntax near WHERE. Use equal sign.";
+                int idx = tsvData.getHeaderIdx(identifier);
+
+                if (!( // catch invalid type
+                    tokens[i_parser+2].type.equals("STRING") || 
+                    tokens[i_parser+2].type.equals("NUMBER")
+                )) return "Invalid syntax near WHERE. Invalid input. Make sure to use numbers or enclose characters in quotation marks.";
+                
+                if (idx == -1) System.out.println("header \"" + identifier + "\" not found. Skipping.");
+                else { // check first if value is a string or number. Then add.
+                    Object[] filterValues = new Object[2];
+                    filterValues[0] = idx;
+                    filterValues[1] = value;
+
+                    filterRow.add(filterValues);
+                }
+
+                i_parser += 3;
+                if (i_parser < tokens.length && tokens[i_parser].type == "COMMA") {
+                    i_parser++;
+                    if ( // detect invalid syntax after comma
+                        i_parser < tokens.length &&
+                        !(tokens[i_parser].type == "IDENTIFIER")
+                    ) return "Invalid syntax near WHERE. Trailing comma.";
+                } else break;
+            }
+        }
+
+        // Construct object table
+        ArrayList<Object[]> modifiedRows = new ArrayList<>();
+
+        for (int i = 0; i < tsvData.data.size(); i++) {
+            String[] row = tsvData.data.get(i);
+            boolean match = true;
+
+            for (int j = 0; j < filterRow.size(); j++) {
+                int idx = (int) filterRow.get(j)[0];
+                String value = (String) filterRow.get(j)[1];
+
+                if (!row[idx].equals(value)) {
+                    match = false;
+                    break;
+                }
+            }
+            
+            if (!match) continue;
+            int[] columnIndices = new int[Values.size()];
+            String[] values = new String[Values.size()];
+
+            for (int j = 0; j < Values.size(); j++) {
+                columnIndices[j]  = (int) Values.get(j)[0];
+                values[j] = (String) Values.get(j)[1];
+            }
+
+            Object[] newObj = new Object[]{i, columnIndices, values};
+            modifiedRows.add(newObj);
+        }
+
+        if (modifiedRows.size() == 0) return "No data modified.";
+
+        tsvData.modify(modifiedRows.toArray(new Object[0][]));// modify 
+
+
+        return "MODIFY success.";
     }
 }
